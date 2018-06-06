0e73068061bbd67e61d15c578da5720d4db3d5fc
sfMaia <mnvinsf@gmail.com>
2018-06-05 19:09:15 -0700

    uploaded css and js files

---
+++ mobile.js
@@ -0,0 +1,8 @@
+function myFunction(x) {
+	x.classList.toggle("change");
+}
+// $(document).ready(function(){
+// 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+// 		$(this).toggleClass('open');
+// 	});
+// });
